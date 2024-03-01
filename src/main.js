156a06cb2b6acc9f74dd9c95d7b2d217a0872b42
Mongrus <artemromanowich90@gmail.com>
2024-03-01 18:00:09 +0300

    rename title

--- src/main.js
+++ src/main.js
@@ -3,4 +3,7 @@ import './assets/main.sass';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+const app = createApp(App);
+app.config.devtools = true;
+
 createApp(App).mount('#app');
